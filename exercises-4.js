5a0e4523454857e8002b35b4dd08ccf2bbc88245
jumadimuhammad <getjumadi@gmail.com>
2020-01-28 16:25:30 +0700

    updat exercises-4.js

--- exercises-4.js
+++ exercises-4.js
@@ -1,10 +1,172 @@
-var tanggal = 29
+var tanggal = 31
 var bulan = 2
 var tahun = 2020
 
 
 switch(tanggal){
     case 1:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 2:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 3:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 4:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 5:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 6:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 7:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 8:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 9:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 10:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 11:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 12:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 13:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 14:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 15:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 16:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 17:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 18:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 19:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 20:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 21:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 22:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 23:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 24:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 25:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 26:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 27:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 28:{
         console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
         break
 
