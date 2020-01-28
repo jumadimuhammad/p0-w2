f1c19eaad9716c6c40dd437466da3b3048bc3644
jumadimuhammad <getjumadi@gmail.com>
2020-01-28 14:52:09 +0700

    update exercises-4.js

---
+++ exercises-4.js
@@ -0,0 +1,22 @@
+var tanggal = 29
+var bulan = 2
+var tahun = 2020
+
+
+switch(tanggal){
+    case 1:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    case 29:{
+        console.log('Tanggal ' + tanggal + ' bulan  ' + bulan + ' tahun ' + tahun )
+        break
+
+    }
+
+    default:{
+        console.log('Tanggal ' + tanggal + ' tidak tersedia')
+    }
+}
