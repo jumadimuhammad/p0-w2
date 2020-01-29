ac2c280a397c3ae68e55229b897cfc272a58b57c
jumadimuhammad <getjumadi@gmail.com>
2020-01-29 20:35:27 +0700

    update exercises.js

--- exercises-6.js
+++ exercises-6.js
@@ -1,5 +1,6 @@
 // 1. LOOPING DENGAN WHILE
 
+
 var i = 2
 console.log('LOOPING PERTAMA')
 while (i <= 20){
@@ -19,9 +20,13 @@ while (i >= 2){
     i-=2
 }
 
-console.log('PERULANGAN MENGGUNAKAN FOR')
+console.log('\n PERULANGAN MENGGUNAKAN FOR \n')
+
+
 
 // 2. MELAKUKAN LOOPING MENGGUNAKAN FOR
+
+
 
 console.log('LOOPING PERTAMA')
 for(var i = 1; i <= 20; i++){
@@ -34,11 +39,13 @@ for(var i = 20; i >= 1; i--){
 }
 
 
+
+
 // 3 ANGKA GANJIL DAN GENAP
 
-
+console.log('\n')
 console.log('ANGKA GANJIL DAN GENAP')
-
+console.log('\n')
 
 for (var i = 1; i <= 100; i++){
 
@@ -46,6 +53,50 @@ for (var i = 1; i <= 100; i++){
         console.log(i + ' Bilangan Genap')
     }else{
         console.log(i + ' Bilangan Ganjil')
+    }
+
+}
+
+
+console.log('\n')
+console.log('PERTAMBAHAN COUNTER 2 KELIPATAN 3 \n')
+
+for (var i = 1; i <= 100; i+=2){
+
+    var temp = i * 3
+
+    if (i % 3 == 0){
+        console.log(i + ' Kelipatan 3')
+    }else{
+        console.log(i)
+    }
+
+}
+
+console.log('\n')
+console.log('PERTAMBAHAN COUNTER 5 KELIPATAN 6 \n')
+
+for (var i = 1; i <= 100; i+=5){
+
+
+    if (i % 6 == 0){
+        console.log(i + ' Kelipatan 6')
+    }else{
+        console.log(i)
+    }
+
+}
+
+console.log('\n')
+console.log('PERTAMBAHAN COUNTER 9 KELIPATAN 10 \n')
+
+for (var i = 1; i <= 100; i+=9){
+
+
+    if (i % 10 == 0){
+        console.log(i + ' Kelipatan 10')
+    }else{
+        console.log(i)
     }
 
 }
